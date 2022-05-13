956ff7570e170bebdb848c27d39c5d26bdceb045
Khurramjaved <kjaved@ualberta.ca>
2022-05-13 17:00:06 -0600

    Add comments to the code

--- robust_linear_learner.cpp
+++ robust_linear_learner.cpp
@@ -10,100 +10,73 @@
 #include "include/utils.h"
 #include "include/environments/supervised_tracking.h"
 #include "include/learner.h"
+#include "include/network_factory.h"
 
 int main(int argc, char *argv[]) {
 
   Experiment *my_experiment = new ExperimentJSON(argc, argv);
 
-  int STEPS = my_experiment->get_int_param("steps");
-  int FEATURES = my_experiment->get_int_param("features");
-  float target_noise = my_experiment->get_float_param("target_noise");
-  float avg_running_error = 0;
+  int T = my_experiment->get_int_param("steps");
+  int no_of_features = my_experiment->get_int_param("features");
+  float noise_in_the_target = my_experiment->get_float_param("target_noise");
 
+//  Initialize database tables
   Metric error_metric = Metric(my_experiment->database_name, "error_table",
-                               std::vector<std::string>{"run", "step", "seed", "sum_of_distance", "end_distance",
-                                                        "sum_of_errors", "asymptotic_error"},
-                               std::vector<std::string>{"int", "int", "int", "real", "real", "real", "real"},
+                               std::vector<std::string>{"run", "step", "seed",
+                                                        "mean_sum_of_errors", "asymptotic_error"},
+                               std::vector<std::string>{"int", "int", "int", "real", "real"},
                                std::vector<std::string>{"run", "step", "seed"});
+
+//  Repeat experiment for seed number of times
   for (int seed = 0; seed < my_experiment->get_int_param("seeds"); seed++) {
+//    Initialize the environment
     SupervisedTracking *env;
     env = new SupervisedTracking(my_experiment->get_float_param("input_mean"),
                                  my_experiment->get_float_param("input_std"),
                                  my_experiment->get_float_param("target_mean"),
                                  my_experiment->get_float_param("target_std"),
-                                 FEATURES,
+                                 no_of_features,
                                  seed,
                                  my_experiment->get_float_param("target_noise"));
 
-    float avg_target = 0;
-    float last_distance = 0;
-    float sum_of_squared_avg_distance = 0;
     float sum_of_error = 0;
-    float last_10k_error = 0;
-    float variance = 0;
-    Learner *network;
-    if (my_experiment->get_string_param("algorithm") == "nadaline")
-      network = new Nadaline(my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "lms")
-      network = new LMS(my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "lms_normalized_step_size")
-      network = new LMSNormalizedStepSize(my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "lms_normalized_step_size_and_input")
-      network = new LMSNormalizedInputsAndStepSizes(my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "normalized_lms")
-      network = new NormalizedLMS(my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "adam")
-      network = new AdamLMS(my_experiment->get_float_param("step_size"),
-                            FEATURES,
-                            my_experiment->get_float_param("b1"),
-                            my_experiment->get_float_param("b2"),
-                            my_experiment->get_float_param("epsilon"));
-    else if (my_experiment->get_string_param("algorithm") == "idbd")
-      network = new IDBD(my_experiment->get_float_param("meta_step_size"), my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "nidbd1")
-      network = new NIDBD1(my_experiment->get_float_param("meta_step_size"), my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "nidbd2")
-      network = new NIDBD2(my_experiment->get_float_param("meta_step_size"), my_experiment->get_float_param("step_size"), FEATURES);
-    else if (my_experiment->get_string_param("algorithm") == "idbdbest")
-      network = new IDBDBest(my_experiment->get_float_param("meta_step_size"), my_experiment->get_float_param("step_size"), FEATURES);
+    float last_20k_steps_error = 0;
+    Learner *network = NetworkFactory::get_learner(my_experiment);
 
-    for (int step = 0; step < STEPS; step++) {
+    for (int step = 0; step < T; step++) {
+//      Get next sample from the world
       auto x = env->step();
+//      Make a prediction
       float pred = network->forward(x);
+//      Get target/label given by the underlying target function after the agent has made the prediction
       float target = env->get_y();
-//      std::cout << "target = " << target << std::endl;
-//      std::cout << "pred = " << pred << std::endl;
-      avg_target = 1.0 / (float(step) + 1.0) * (target + float(step) * avg_target);
-      float mse = (target - pred) * (target - pred);
-      if (STEPS - step < 20000) {
-        int step_counter = 20000 - (STEPS - step);
-        last_10k_error = 1.0 / (step_counter)
-            * (network->distance_to_target_weights(env->get_target_weights()) + (step_counter - 1) * last_10k_error);
-      }
-//      std::cout << "last 20 k = " << last_10k_error << std::endl;
-      sum_of_error += mse;
+//      Compute the squared error
+      float squared_error = (target - pred) * (target - pred);
+//      Set the gradient accumulation vector to zero
       network->zero_grad();
+//      Add gradient of the 1/2 (target - pred)^2 w.r.t the learnable parameters to the gradient accumulation vector
       network->backward(x, pred, target);
-      if(step > 3000)
-        network->update_parameters();
-      last_distance = network->distance_to_target_weights(env->get_target_weights());
-      sum_of_squared_avg_distance += last_distance;
+//      Update the parameters using the stored gradients
+      network->update_parameters();
+//      Update the sum of errors so far
+      sum_of_error += squared_error;
+//      Update asymptotic error (Error on last 20k steps).
+      if (T - step < 20000) {
+        int step_counter = 20000 - (T - step);
+        last_20k_steps_error = 1.0 / (step_counter)
+            * (network->distance_to_target_weights(env->get_target_weights())
+                + (step_counter - 1) * last_20k_steps_error);
+      }
     }
 
+//    Push results in the database
     std::vector<std::string> cur_error;
     cur_error.push_back(std::to_string(my_experiment->get_int_param("run")));
-    cur_error.push_back(std::to_string(STEPS));
+    cur_error.push_back(std::to_string(T));
     cur_error.push_back(std::to_string(seed));
-    cur_error.push_back(std::to_string(sum_of_squared_avg_distance / STEPS));
-    cur_error.push_back(std::to_string(last_distance));
-    cur_error.push_back(std::to_string(sum_of_error / STEPS));
-    cur_error.push_back(std::to_string(last_10k_error));
+    cur_error.push_back(std::to_string(sum_of_error / T));
+    cur_error.push_back(std::to_string(last_20k_steps_error));
     error_metric.record_value(cur_error);
-//    LMSNormalizedInputsAndStepSizes* ptr = dynamic_cast<LMSNormalizedInputsAndStepSizes*>(network);
-//    std::cout << "Mean\n";
-//    print_vector(ptr->input_normalization_mean);
-//    std::cout << "Variance\n";
-//    print_vector(ptr->input_normalization_std);
   }
 
   error_metric.commit_values();
